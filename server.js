e887ce2057b65964c5119d3d8ebcebf2f7dfa6d0
mozahid679 <mozahid679@gmail.com>
2022-06-01 09:17:42 +0600

    setup for almost all types of backend projects

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const http = require('http');
+const app = require('./app/app')
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT ||8000
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+
+})
+
+// video 16 2:11:34
